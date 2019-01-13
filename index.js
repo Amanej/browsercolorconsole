eb187314f50b445e17f476494248af78264d62da
Aman <aman.mender@tegningtil3d.no>
2019-01-13 20:56:27 +0100

    Basic functionality

---
+++ index.js
@@ -0,0 +1,21 @@
+const browserLogColors = {
+    color:(string,hexcode,bgcolor) => {
+        let base = [`${'%c '}${string}`]; 
+        if(hexcode && bgcolor) {
+            base.push(`background:${bgcolor} ; color: ${hexcode}`)
+        } else if (hexcode && !bgcolor) {
+            base.push(`color: ${hexcode}`)
+        } else if (!hexcode && bgcolor) {
+            base.push(`background: ${hexcode}`)
+        }
+        return base
+    },
+    log:(string,hexcode,bgcolor) => {
+        let coloredString = browserLogColors.color(string,hexcode,bgcolor);
+        if(coloredString.length > 1) {
+            console.log(coloredString[0],coloredString[1]);
+        } else {
+            console.log(coloredString[0]);
+        }
+    }
+}
